c2e77a8d2cc8524152f9de5c320d53044c2015d3
Maxim <maxim.yakimaha@gmail.com>
2021-09-19 13:44:57 +0300

    Issue TypeError: Scalar tensor has no `len()` [in y_train_uni]

--- singleTime.py
+++ singleTime.py
@@ -15,20 +15,22 @@ df = pd.read_csv(csv_path)
 
 TRAIN_SPLIT = 200
 
+
 def univariate_data(dataset, start_index, end_index, history_size, target_size):
-  data = []
-  labels = []
+    data = []
+    labels = []
 
-  start_index = start_index + history_size
-  if end_index is None:
-    end_index = len(dataset) - target_size
+    start_index = start_index + history_size
+    if end_index is None:
+        end_index = len(dataset) - target_size
 
-  for i in range(start_index, end_index):
-    indices = range(i-history_size, i)
-    # Reshape data from (history_size,) to (history_size, 1)
-    data.append(np.reshape(dataset[indices], (history_size, 1)))
-    labels.append(dataset[i+target_size])
-  return np.array(data), np.array(labels)
+    for i in range(start_index, end_index):
+        indices = range(i - history_size, i)
+        # Reshape data from (history_size,) to (history_size, 1)
+        data.append(np.reshape(dataset[indices], (history_size, 1)))
+        labels.append(dataset[i + target_size])
+    return np.array(data), np.array(labels)
+
 
 tf.random.set_seed(13)
 
@@ -40,11 +42,11 @@ uni_data = uni_data.values
 uni_train_mean = uni_data[:TRAIN_SPLIT].mean()
 uni_train_std = uni_data[:TRAIN_SPLIT].std()
 
-uni_data = (uni_data-uni_train_mean)/uni_train_std
+uni_data = (uni_data - uni_train_mean) / uni_train_std
 
-
-univariate_past_history = 100
-univariate_future_target = 0
+univariate_past_history = 193
+univariate_future_target = 1
+STEP = 1
 
 x_train_uni, y_train_uni = univariate_data(uni_data, 0, TRAIN_SPLIT,
                                            univariate_past_history,
@@ -53,34 +55,35 @@ x_val_uni, y_val_uni = univariate_data(uni_data, TRAIN_SPLIT, None,
                                        univariate_past_history,
                                        univariate_future_target)
 
-#print ('Single window of past history')
-#print (x_train_uni.shape)
-#print ('\n Target temperature to predict')
-#print (y_train_uni.shape)
+print('Single window of past history : {}'.format(x_train_uni[0].shape))
+print('\n Target temperature to predict : {}'.format(y_train_uni[0].shape))
+
 
 def create_time_steps(length):
-  return list(range(-length, 0))
+    return list(range(-length, 0))
+
 
 def show_plot(plot_data, delta, title):
-  labels = ['History', 'True Future', 'Model Prediction']
-  marker = ['.-', 'rx', 'go']
-  time_steps = create_time_steps(plot_data[0].shape[0])
-  if delta:
-    future = delta
-  else:
-    future = 0
-
-  plt.title(title)
-  for i, x in enumerate(plot_data):
-    if i:
-      plt.plot(future, plot_data[i], marker[i], markersize=10,
-               label=labels[i])
+    labels = ['History', 'True Future', 'Model Prediction']
+    marker = ['.-', 'rx', 'go']
+    time_steps = create_time_steps(plot_data[0].shape[0])
+    if delta:
+        future = delta
     else:
-      plt.plot(time_steps, plot_data[i].flatten(), marker[i], label=labels[i])
-  plt.legend()
-  plt.xlim([time_steps[0], (future+5)*2])
-  plt.xlabel('Time-Step')
-  return plt.savefig('plot.png')
+        future = 0
+
+    plt.title(title)
+    for i, x in enumerate(plot_data):
+        if i:
+            plt.plot(future, plot_data[i], marker[i], markersize=10,
+                     label=labels[i])
+        else:
+            plt.plot(time_steps, plot_data[i].flatten(), marker[i], label=labels[i])
+    plt.legend()
+    plt.xlim([time_steps[0], (future + 5) * 2])
+    plt.xlabel('Time-Step')
+    return plt.savefig('plot.png')
+
 
 #show_plot([x_train_uni[0], y_train_uni[0]], 0, 'Sample Example')
 
@@ -95,31 +98,40 @@ train_univariate = train_univariate.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZ
 val_univariate = tf.data.Dataset.from_tensor_slices((x_val_uni, y_val_uni))
 val_univariate = val_univariate.batch(BATCH_SIZE).repeat()
 
+print(val_univariate.take(1))
 
-simple_lstm_model = tf.keras.models.Sequential([
-    tf.keras.layers.LSTM(8, input_shape=x_train_uni.shape[-2:]),
-    tf.keras.layers.Dense(1)
+EVALUATION_INTERVAL = 20
+EPOCHS = 1
 
-])
+simple_lstm_model = tf.keras.models.Sequential()
+simple_lstm_model.add(tf.keras.layers.LSTM(32,
+                                           return_sequences=True,
+                                           input_shape=x_train_uni.shape[-2:]))
+simple_lstm_model.add(tf.keras.layers.LSTM(16, activation='relu'))
+simple_lstm_model.add(tf.keras.layers.Dense(7))
 
-simple_lstm_model.compile(optimizer='adam', loss='mae')
+simple_lstm_model.compile(optimizer=tf.keras.optimizers.RMSprop(clipvalue=1.0), loss='mae')
 
-################
+multi_step_history = simple_lstm_model.fit(train_univariate, epochs=EPOCHS,
+                                           steps_per_epoch=EVALUATION_INTERVAL,
+                                           validation_data=val_univariate,
+                                           validation_steps=50)
 
-for x, y in val_univariate.take(0):
-    print(simple_lstm_model.predict(x).shape)
 
-################
+def multi_step_plot(history, true_future, prediction):
+    plt.figure(figsize=(12, 6))
+    num_in = create_time_steps(len(history))
+    num_out = len(true_future)
 
-EVALUATION_INTERVAL = 200
-EPOCHS = 10
+    plt.plot(num_in, np.array(history[:, 1]), label='History')
+    plt.plot(np.arange(num_out) / STEP, np.array(true_future), 'bo',
+             label='True Future')
+    if prediction.any():
+        plt.plot(np.arange(num_out) / STEP, np.array(prediction), 'ro',
+                 label='Predicted Future')
+    plt.legend(loc='upper left')
+    plt.show()
 
-simple_lstm_model.fit(train_univariate, epochs=EPOCHS,
-                      steps_per_epoch=EVALUATION_INTERVAL,
-                      validation_data=val_univariate, validation_steps=50)
 
 for x, y in val_univariate.take(1):
-  plot = show_plot([x[0].numpy(), y[0].numpy(),
-                    simple_lstm_model.predict(x)[0]], 0, 'Simple LSTM model')
-
-
+    multi_step_plot(x[0], y[0], np.array([0]))
